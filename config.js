448de8f079b6d69161ad479855bb98db0bf6198b
dvhop <dvhopptit@gmail.com>
2023-03-08 00:34:24 +0700

    Add files via upload
    init

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Iu em';
+const giftImage = 'panh2.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
